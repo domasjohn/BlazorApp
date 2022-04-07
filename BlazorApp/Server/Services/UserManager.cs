94c2e798e4d96dc7043b8d17c0a9bae8572ba216
Domas Jonaitis <domasjon16@gmail.com>
2022-04-07 18:45:38 +0300

    Add project files.

---
+++ BlazorApp/Server/Services/UserManager.cs
@@ -0,0 +1,95 @@
+﻿using BlazorApp.Server.Interfaces;
+using BlazorApp.Server.Models;
+using BlazorApp.Shared.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorApp.Server.Services
+{
+    public class UserManager : IUser
+    {
+        readonly DatabaseContext _dbContext = new();
+
+        public UserManager(DatabaseContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public List<User> GetUserDetails()
+        {
+            try
+            {
+                return _dbContext.Users.ToList();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public void AddUser(User user)
+        {
+            try
+            {
+                _dbContext.Users.Add(user);
+                _dbContext.SaveChanges();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+        
+        public void UpdateUserDetails(User user)
+        {
+            try
+            {
+                _dbContext.Entry(user).State = EntityState.Modified;
+                _dbContext.SaveChanges();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+        
+        public User GetUserData(int id)
+        {
+            try
+            {
+                User? user = _dbContext.Users.Find(id);
+                if (user != null)
+                {
+                    return user;
+                }
+                else
+                {
+                    throw new ArgumentNullException();
+                }
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public void DeleteUser(int id)
+        {
+            try
+            {
+                User? user = _dbContext.Users.Find(id);
+                if (user != null)
+                {
+                    _dbContext.Users.Remove(user);
+                    _dbContext.SaveChanges();
+                }
+                else
+                {
+                    throw new ArgumentNullException();
+                }
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
